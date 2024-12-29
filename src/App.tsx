6841b00fc47b3a15ff996b8d57ad49b06e75b82c
Minchan <98478661+itscold96@users.noreply.github.com>
2024-12-29 21:59:56 +0900

    tailwind 설치 및 기반 변수 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-  return <div>App</div>;
+  return <div className={'bg-primary h-screen w-screen text-white'}>App</div>;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import '@fontsource/noto-sans-kr/400.css';
+import '@fontsource/noto-sans-kr/700.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
